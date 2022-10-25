8c904145a4ed455d5361de5d80c2095ccdeb52d9
abhi12-ayalur <abhinav.ayalur@gmail.com>
2022-10-25 06:41:36 +0000

    refactor

--- predictor.py
+++ predictor.py
@@ -1,19 +1,19 @@
 from typing import List, Dict
-from sieve.types import FrameSingleObject, SingleObject, BoundingBox, TemporalObject
-from sieve.predictors import TemporalProcessor
+from sieve.types import FrameSingleObject, SingleObject, BoundingBox, Temporal
+from sieve.predictors import TemporalPredictor
 
 from retinaface import RetinaFace
 import cv2
 
-class FaceDetector(TemporalProcessor):
+class FaceDetector(TemporalPredictor):
     def setup(self):
         self.model = RetinaFace
         a = cv2.imread('dummy.png')
         self.model.detect_faces(a)
     
     def predict(self, frame: FrameSingleObject) -> List[SingleObject]:
-        frame_number = frame.temporal_object.frame_number
-        frame_data = frame.temporal_object.get_array()
+        frame_number = frame.get_temporal().frame_number
+        frame_data = frame.get_temporal().get_array()
         faces = self.model.detect_faces(frame_data)
 
         output_objects = []
@@ -29,7 +29,7 @@ class FaceDetector(TemporalProcessor):
             output_objects.append(
                 SingleObject(
                     cls=out_cls,
-                    temporal_object=TemporalObject(
+                    temporal=Temporal(
                         frame_number=frame_number,
                         bounding_box=out_bbox,
                         score=out_score,
